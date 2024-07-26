a47599cd8b6f0c4841488f3395189ca0db72840b
ayanus <sunaya2855@gmail.com>
2024-07-27 00:41:29 +0700

    สร้างหน้าเพิ่มวัตถุดิบ

--- public/bar.php
+++ public/bar.php
@@ -5,53 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Big Bee Farm</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-        }
-        .sidebar {
-            width: 200px;
-            background-color: #F8F8F8;
-            border-right: 1px solid #E0E0E0;
-            padding: 20px 0;
-            /* top : 200px; */
-        }
-        .sidebar a {
-            display: block;
-            padding: 10px 20px;
-            color: #000;
-            text-decoration: none;
-            transition: background-color 0.3s;
-        }
-        .content {
-            flex-grow: 1;
-            padding: 20px;
-
-        }
-        .topbar {
-            width: 100%;
-            background-color: #fff9c4;
-            border-bottom: 1px solid #E0E0E0;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 10px 20px;
-            position: fixed;
-            top: 0;
-            left: 0;
-            box-sizing: border-box;
-        }
-        .topbar input[type="text"] {
-            width: 300px;
-            padding: 5px;
-            border: 1px solid #E0E0E0;
-            border-radius: 4px;
-        }
     </style>
 </head>
+
 <body>
+    <div class="content">
+        <div class="bar">
+            <div class="logo">
+                <h2 style="text-align:center;">Big Bee Farm</h2>
+            </div>
+        </div>
+    </div>
     <div class="sidebar">
         <a href="#orders">ข้อมูลผึ้ง</a>
         <a href="#locations">การเลี้ยงผึ้ง</a>
@@ -60,13 +24,6 @@
         <a href="#sales">การขาย</a>
         <a href="#products">ผลิตภัณฑ์</a>
         <a href="#suppliers">Supplier</a>
-    </div>
-    <div class="content">
-        <div class="topbar">
-            <div class="logo">
-                <h2 style="text-align:center;">Big Bee Farm</h2>
-            </div>
-        </div>
     </div>
 </body>
 </html>
---
+++ src/views/admin/materials/html/materials.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Materials</title>
+    <link rel="stylesheet" href="src/views/admin/materials/css/material.css">
+</head>
+    <body>
+        <!-- สร้างปุ่มเพิ่มข้อมูลวัตถุดิบ โดยมีการอ้างอิงถึงไฟล์อื่น -->
+        <div class="btn-add">
+            <a href="add.php"><button type="button" class="btn btn-primary">Add</button></a>
+        </div>
+        <div class="table">
+            <table border="1" align="left" width="50%" height="20%">
+                <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Date of order</th>
+                    <th>Quantity</th>
+                </tr>
+        </table>
+        </div>
+    </body>
+</html>
