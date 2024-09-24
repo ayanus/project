8ce19088061f549f3f5f40dd8949add2905ef866
ayanus <sunaya2855@gmail.com>
2024-09-24 18:52:06 +0700

    อัพโหลดรูปได้แล้ว

--- src/owner/employee/showmore_em.php
+++ src/owner/employee/showmore_em.php
@@ -1,4 +1,5 @@
 <?php
+    ob_start();
     include 'C:/xampp/htdocs/project/config/database.php';
     $id = $_GET['employee_id'];
     $sql="SELECT * FROM employee WHERE employee_id = $id";
@@ -15,6 +16,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/project/public/css/style.css">
+<style>
+    .content {
+        display: flex;
+        justify-content: space-between; /* ทำให้ช่องว่างระหว่าง .data และ .profile */
+    }
+
+    .data {
+        width: 60%;
+    }
+    .pro {
+        width: 40%;
+        /* background-color: red; */
+        margin-left: auto; /* ทำให้ชิดขวา */
+    }
+
+    .pro img {
+        width: 50%;
+        height: 50%;
+        margin-top: 30px;
+        border: 2px solid #000;
+    }
+    
+
+</style>
 </head>
     <body>
     <div class="containerr">
@@ -29,7 +54,7 @@
                     <div class="header"><a href="show_em.php"><ion-icon name="chevron-back-outline"></ion-icon></a>ข้อมูลเพิ่มเติม</div>
                         <div class="content">
                             <div class="data">
-                                <h4 class="pb-3">ข้อมูลทั่วไป</h4>
+                                <h5 class="pb-3 fw-semibold">ข้อมูลทั่วไป</h5>
                                 <div class="row mb-3 mt-2 ml-6">
                                     <div class="col-auto">
                                         <label class="col-form-label ">ชื่อ - สกุล</label>
@@ -70,7 +95,7 @@
                                     </div>
                                 </div>
 
-                                <h4 class="pb-3 pt-4">ข้อมูลธนาคาร</h4>
+                                <h5 class="pb-3 pt-4 fw-semibold">ข้อมูลธนาคาร</h5>
                                 <div class="row mb-3 mt-2 ml-6">
                                     <div class="col-auto">
                                         <label class="col-form-label ">ชื่อบัญชี</label>
@@ -97,7 +122,45 @@
                                         <input type="text" name="account_num" class="form-control" value="<?= isset($row['account_num']) ? $row['account_num'] : '' ?>" readonly>
                                     </div>
                                 </div>
+                            </div>
 
+                            <div class="pro">
+                                <div class="text-center">
+                                    <form id="uploadForm" enctype="multipart/form-data" method="post">
+                                        <label for="picture">
+                                            <img id="profileImagePreview" src="/project/public/picture/addpic.png" class="rounded" alt="." style="cursor: pointer;">
+                                        </label>
+                                        <input type="file" id="picture" name="picture" accept="image/*" style="display: none;" onchange="previewImage(event)">
+                                        <button type="submit">อัปโหลด</button>
+                                    </form>
+                                    <img id="profileImagePreview" src="" alt="Profile Image Preview" style="display:none;"/>
+                                    <?php
+                                    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['picture'])) {
+                                        // Same code to handle file upload and database update
+                                        $target_dir = "C:/xampp/htdocs/project/public/picture/";
+                                        $target_file = $target_dir . basename($_FILES["picture"]["name"]);
+                                        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+                                        $check = getimagesize($_FILES["picture"]["tmp_name"]);
+                                        if ($check !== false) {
+                                            if (move_uploaded_file($_FILES["picture"]["tmp_name"], $target_file)) {
+                                                $stmt = $conn->prepare("UPDATE employee SET picture = ? WHERE employee_id = ?");
+                                                $stmt->bind_param("si", $target_file, $id);
+                                                if ($stmt->execute()) {
+                                                    echo "อัปโหลดรูปภาพสำเร็จ";
+                                                } else {
+                                                    echo "เกิดข้อผิดพลาด: " . $conn->error;
+                                                }
+                                            } else {
+                                                echo "เกิดข้อผิดพลาดในการอัปโหลดไฟล์";
+                                            }
+                                        } else {
+                                            echo "ไฟล์นี้ไม่ใช่รูปภาพ";
+                                        }
+                                    }
+                                    ?>
+
+                                </div>
+                            </div>                    
                             </div>
                         </div>    
                 </div>
@@ -107,14 +170,19 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
+    <script language="Javascript">
+        function previewImage(event) {
+            const reader = new FileReader();
+            reader.onload = function() {
+                const output = document.getElementById('profileImagePreview');
+                output.src = reader.result;
+            };
+            reader.readAsDataURL(event.target.files[0]);
+        }
+    </script>
+
+    <?
+    ob_end_flush();
+    ?>
     </body>
 </html>
-
-<script language="Javascript">
-    function Del(mypage){
-        var agree=confirm("คุณต้องการลบข้อมูลนี้ใช่หรือไม่?");
-        if(agree){
-            window.location = mypage;
-        }
-    }
-</script>
